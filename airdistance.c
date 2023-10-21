8a82f53d7fbdc066227724b68efc4e878ea365f4
Saifullahfarrukh <145596935+Saifullahfarrukh@users.noreply.github.com>
2023-10-21 06:26:11 -0700

    1st change in airdistance.c

---
+++ airdistance.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+#define earth_radius 6371.0
+#define M_PI 3.14159265358979323846
+
+double degreesToRadians(double degrees) {
+    return degrees * M_PI / 180.0;
+
+}
+
