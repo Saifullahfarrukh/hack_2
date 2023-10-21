668a407471355a60107e2c314a3dc10dedda7876
Saifullahfarrukh <145596935+Saifullahfarrukh@users.noreply.github.com>
2023-10-21 06:32:53 -0700

    2nd

--- airdistance.c
+++ airdistance.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+
 #include <math.h>
 
 #define earth_radius 6371.0
+
 #define M_PI 3.14159265358979323846
 
 double degreesToRadians(double degrees) {
+
     return degrees * M_PI / 180.0;
 
 }
 
+int main() {
+
+//declaring variables
+
+    double lat1 , long1 , lat2 , long2;
+// 
+
+const earth_radius = 6371.0;
+
+
+}
